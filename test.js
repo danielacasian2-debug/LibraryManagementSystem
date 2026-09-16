f236ec1cfeea2abad57d2c1463b499c6da84e775
danielacasian2-debug <danielacasian2@gmail.com>
2026-09-16 15:23:57 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+let books = [
+    {
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        available: true
+    },
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        available: false
+    },
+    {
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        available: true
+    }
+];
+
+
+function addBook(book){
+ books.push(book);
+ 
+}
